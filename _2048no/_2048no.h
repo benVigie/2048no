34c1cf2bf3768eaf1baded1d5ff785f5dd484823
Benjamin Vigie <vigie.benjamin@gmail.com>
2014-09-13 18:59:25 +0200

    Adding Directions enum Plug joystick to the project to easily debug the game

---
+++ _2048no/_2048no.h
@@ -0,0 +1,31 @@
+#ifndef __2048NO__
+#define __2048NO__
+
+// NeoMatrix control pin
+const int MATRIC_PIN  = 6;
+// Pin for joystick, test purpose only - To be remove when we will use gyroscop 
+const int VERT_PIN    = 5;
+const int HORIZ_PIN   = 4;
+
+// Instanciate the Led Matrix
+// See https://learn.adafruit.com/adafruit-neopixel-uberguide/neomatrix-library
+Adafruit_NeoMatrix _matrix = Adafruit_NeoMatrix(8, 8, MATRIC_PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB            + NEO_KHZ800);
+
+// Game grid, a 4x4 square
+int _grid[16];
+
+// Boolean use by the joystick
+bool  isDefaultPosition = true;
+
+enum Direction {
+  NoDirection,
+  Up,
+  Right,
+  Down,
+  Left
+};
+
+#endif
--- _2048no/_2048no.ino
+++ _2048no/_2048no.ino
@@ -2,23 +2,16 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
+#include "_2048no.h"
 
-// See https://learn.adafruit.com/adafruit-neopixel-uberguide/neomatrix-library
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, 6,
-  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
-  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
-  NEO_GRB            + NEO_KHZ800);
-
-// Game grid, a 4x4 square
-int grid[16];
 
 void setup() {
   // Initialyze matrix
-  matrix.begin();
-  matrix.setBrightness(20);
+  _matrix.begin();
+  _matrix.setBrightness(20);
   
   // Initialyze grid and random
-  memset(grid, 0, 16);
+  memset(_grid, 0, 16);
   randomSeed(analogRead(0));
   
   // Insert 2 number to start the game
@@ -33,6 +26,27 @@ void setup() {
 }
 
 void loop() {
+  Direction direction;
+
+  // Get the player direction
+  direction = checkMove();
+  switch (direction) {
+    case Up:
+      Serial.println("Go to: Up");
+      break;
+    case Right:
+      Serial.println("Go to: Right");
+      break;
+    case Down:
+      Serial.println("Go to: Down");
+      break;
+    case Left:
+      Serial.println("Go to: Left");
+      break;
+    default:
+      ;
+  }
+
   //printGrid();
 }
 
@@ -43,20 +57,20 @@ void printGrid() {
   uint16_t color;
   int line, col;
   
-  matrix.fillScreen(0);
+  _matrix.fillScreen(0);
   
   for (int i = 0; i < 16; i++){
-    color = getColor(grid[i]);
+    color = getColor(_grid[i]);
     line = i / 4;
     col = i % 4;
     
-    matrix.drawPixel(col * 2, line * 2, color);
-    matrix.drawPixel(col * 2 + 1, line * 2, color);
-    matrix.drawPixel(col * 2, line * 2 + 1, color);
-    matrix.drawPixel(col * 2 + 1, line * 2 + 1, color);
+    _matrix.drawPixel(col * 2, line * 2, color);
+    _matrix.drawPixel(col * 2 + 1, line * 2, color);
+    _matrix.drawPixel(col * 2, line * 2 + 1, color);
+    _matrix.drawPixel(col * 2 + 1, line * 2 + 1, color);
   }
   
-  matrix.show();
+  _matrix.show();
 }
 
 /**
@@ -67,13 +81,13 @@ uint16_t getColor(int value) {
   
   switch (value) {
     case 2:
-      color = matrix.Color(255, 0, 0);
+      color = _matrix.Color(255, 0, 0);
       break;
     case 4:
-      color = matrix.Color(0, 255, 0);
+      color = _matrix.Color(0, 255, 0);
       break;
     default:
-      color = matrix.Color(0, 0, 0);
+      color = _matrix.Color(0, 0, 0);
   }
   
   return (color);
@@ -84,14 +98,44 @@ void insertNumber() {
   
   long number = random(100);
   
-  while (grid[index] != 0) {
+  while (_grid[index] != 0) {
     index = random(16);
   }
   
   if (number < 66)
-    grid[index] = 2;
+    _grid[index] = 2;
   else
-    grid[index] = 4;
+    _grid[index] = 4;
 }
 
+/**
+ *  Return the joystick position, one of Direction enum
+ */
+Direction checkMove() {
+  int       vertical, horizontal;
+  Direction currentDirection      = NoDirection;
 
+  // Move detection
+  vertical = map(analogRead(VERT_PIN), 0, 1023, 0, 5);
+  horizontal = map(analogRead(HORIZ_PIN), 0, 1023, 0, 5);
+
+  // Check if the joystick is bent to a direction
+  if (isDefaultPosition) {
+    if ((vertical == 0) || (vertical == 4) || (horizontal == 0) || (horizontal == 4)) {
+      isDefaultPosition = false;
+      if (vertical == 0)
+        currentDirection = Up;
+      else if (vertical == 4)
+        currentDirection = Down;
+      else if (horizontal == 0)
+        currentDirection = Left;
+      else
+        currentDirection = Right;
+    }
+  }
+  // Else check if the joystick is back to its default position
+  else if ((!isDefaultPosition) && (vertical == 2) && (horizontal == 2))
+    isDefaultPosition = true;
+
+  return (currentDirection);
+}
